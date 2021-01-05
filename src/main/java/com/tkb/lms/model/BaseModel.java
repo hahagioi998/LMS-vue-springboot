5a0a35f3ba259eb5890539943ff394ed4431753c
bigkenbird <k918231524@gmail.com>
2021-01-05 23:24:23 +0800

    新增BaseModel

---
+++ src/main/java/com/tkb/lms/model/BaseModel.java
@@ -0,0 +1,19 @@
+package com.tkb.lms.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BaseModel {
+	
+	private String create_by;
+	
+	private String update_by;
+	
+	private String create_time;
+	
+	private String update_time;
+
+	
+}
